4b43c3ea7c9eab56f98b846923c40ab2cc62ca36
Halima95 <Halimaalshahrani@gmail.com>
2020-12-28 18:02:57 +0300

    Adding logo and links

--- portfolio.js
+++ portfolio.js
@@ -24,12 +24,12 @@ $('a[href*="#"]')
                     // Must change focus!
                     var $target = $(target);
                     $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
+                    // if ($target.is(":focus")) { // Checking if the target was focused
+                    //     return false;
+                    // } else {
+                    //     $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                    //     $target.focus(); // Set focus again
+                    // };
                 });
             }
         }
